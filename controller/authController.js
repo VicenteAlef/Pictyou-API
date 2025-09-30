f97eaacd4f048417cd47ae9f5d32f62682e59d38
Vicente Silva <ildealef@outlook.com>
2025-09-30 20:01:01 -0300

    Creation of the definitive database; Password hashing and data processing applied.

--- controller/authController.js
+++ controller/authController.js
@@ -21,10 +21,17 @@ const authController = {
 
   async createUser(req, res) {
     try {
-      const { nome, email } = req.body;
+      const { username, email, password } = req.body;
 
-      if (!nome || nome.trim() === "") {
+      if (!username || username.trim() === "") {
         return res.status(400).json({ message: "Nome é obrigatório" });
+      }
+
+      const existingUser = await User.findByEmail(email);
+      if (existingUser) {
+        return res.status(409).json({
+          message: "Email já está cadastrado",
+        });
       }
 
       const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -32,8 +39,17 @@ const authController = {
         return res.status(400).json({ message: "Email inválido" });
       }
 
-      const newUser = User.createUser(nome, email);
-      res.status(201).json({ message: "Usuário criado", newUser });
+      const passwordRegex =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/;
+      if (!passwordRegex.test(password)) {
+        return res.status(400).json({
+          message:
+            "Senha deve ter no mínimo 8 caracteres, incluindo letra maiúscula, minúscula, número e caractere especial (@$!%*?&)",
+        });
+      }
+
+      const newUser = await User.createUser(username, email, password);
+      res.status(201).json({ newUser });
     } catch (error) {
       console.error("Erro ao criar usuário: ", error);
       return res
--- models/User.js
+++ models/User.js
@@ -1,17 +1,32 @@
 import pool from "../config/database.js";
+import bcrypt from "bcryptjs";
 
 class User {
   static async listUsers() {
-    const [rows] = await pool.query("SELECT * FROM users");
+    const [rows] = await pool.query("SELECT id, username, email FROM users");
     return rows;
   }
 
-  static async createUser(nome, email) {
+  static async findByEmail(email) {
+    try {
+      const [rows] = await pool.query(
+        "SELECT email FROM users WHERE email = ?",
+        [email]
+      );
+      return rows[0] || null; // Retorna o usuário ou null se não existir
+    } catch (error) {
+      console.error("Erro ao buscar usuário por email:", error);
+      throw error;
+    }
+  }
+
+  static async createUser(username, email, password) {
+    const hashedPassword = await bcrypt.hash(password, 10);
     const [result] = await pool.query(
-      "INSERT INTO users (nome, email) VALUES (?, ?)",
-      [nome, email]
+      "INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)",
+      [username, email, hashedPassword]
     );
-    return { id: result.insertId, nome, email };
+    return { id: result.insertId, username, email };
   }
 }
 
